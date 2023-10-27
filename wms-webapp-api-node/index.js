9189f1ac50f4a156cbdc90bdb2615ff17e896d58
Avinash Joshi <avinash.joshi1@gmail.com>
2023-10-28 01:30:09 +0530

    add index web page and add new apis

--- wms-webapp-api-node/index.js
+++ wms-webapp-api-node/index.js
@@ -4,6 +4,10 @@ const sqlite3 = require('sqlite3').verbose();
 
 // Create an Express app
 const app = express();
+app.use(express.static('public'))
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
 
 // Create a SQLite database object
 const db = new sqlite3.Database('./db/wms-db.db', (err) => {
@@ -55,6 +59,41 @@ app.get('/devicelog', (req, res) => {
 
 });
 
+app.get('/devicestatus/:device_id', (req, res) => {
+
+    let sqlQuery = `select device_status  from deviceinfo where device_id=${req.params.device_id}`;
+
+    db.all(sqlQuery, (err, rows) => {
+        if (err) {
+            // Send an error response if there is any
+            res.status(500).send(err.message);
+        } else {
+            // Send the rows as a JSON array if successful
+            res.json(rows);
+        }
+    });
+});
+
+app.post('/devicestatus/:device_id/:status', (req, res) => {
+
+    let sqlQuery = `update deviceinfo set device_status= ${req.params.status} where device_id=${req.params.device_id}`;
+
+    db.all(sqlQuery, (err, rows) => {
+        if (err) {
+            // Send an error response if there is any
+            res.status(500).send(err.message);
+        } else {
+
+
+            insertDevicelog(req.params.device_id, req.params.status)
+            // Send the rows as a JSON array if successful
+            res.json(rows);
+        }
+    });
+
+});
+
+
 /*
     Route path: /users/:userId/books/:bookId
     Request URL: http://localhost:3000/users/34/books/8989
@@ -66,16 +105,16 @@ app.get('/devicelog', (req, res) => {
 
 //http://localhost:3000/devicelog/1/2023-10-27/false/07:50:56/07:51:56
 app.post('/devicelog/:device_id/:creation_date/:device_status/:device_starttime/:device_stoptime', (req, res) => {
-console.log(req.params)
+    console.log(req.params)
 
- let device_id= req.params.device_id;
- let creation_date= req.params.creation_date;
- let device_status= req.params.device_status;
- let  device_starttime= req.params.device_starttime;
- let device_stoptime= req.params.device_stoptime;
+    let device_id = req.params.device_id;
+    let creation_date = req.params.creation_date;
+    let device_status = req.params.device_status;
+    let device_starttime = req.params.device_starttime;
+    let device_stoptime = req.params.device_stoptime;
 
-let sqlQuery=`INSERT INTO devicelogger (device_id, creation_date, device_status, device_starttime, device_stoptime) VALUES ( ${device_id},'${creation_date}',${device_status}, '${device_starttime}','${device_stoptime}');`
-console.log(sqlQuery);
+    let sqlQuery = `INSERT INTO devicelogger (device_id, creation_date, device_status, device_starttime, device_stoptime) VALUES ( ${device_id},'${creation_date}',${device_status}, '${device_starttime}','${device_stoptime}');`
+    console.log(sqlQuery);
 
     db.run(sqlQuery, (err) => {
         if (err) {
@@ -90,6 +129,66 @@ console.log(sqlQuery);
     res.json(req.params)
 });
 
+function insertDevicelog(device_id, device_status) {
+
+    // current timestamp in milliseconds
+    let ts = Date.now();
+
+    let date_ob = new Date(ts);
+    let date = date_ob.getDate();
+    let month = date_ob.getMonth() + 1;
+    let year = date_ob.getFullYear();
+
+    // prints date & time in YYYY-MM-DD format
+    console.log(year + "-" + month + "-" + date);
+    device_starttime = `${date_ob.getHours()}:${date_ob.getMinutes()}:${date_ob.getSeconds()}`
+    creation_date = `${year}-${month}-${date}`
+    console.log(creation_date);
+    console.log(device_starttime);
+    console.log(`  device Status -  ${device_status}`);
+    try {
+
+        let sqlQuery = `INSERT INTO devicelogger (device_id, creation_date, device_status, device_starttime, device_stoptime) VALUES ( ${device_id},'${creation_date}',${device_status}, '${device_starttime}','');`
+        console.log(sqlQuery);
+
+        db.run(sqlQuery, (err) => {
+            if (err) {
+                // Send an error response if there is any
+                return err.message;
+            } else {
+                // Send the rows as a JSON array if successful
+                return this.lastID;
+            }
+        });
+
+    } catch (error) {
+        console.log(error.message);
+    }
+};
+
+// index page
+app.get('/', function (req, res) {
+    
+    var data=[];
+    db.all('select * from devicelogger order by id desc limit 10', (err, rows) => {
+        if (err) {
+            // Send an error response if there is any
+            console.log(err.message);
+        } else {
+            // Send the rows as a JSON array if successful
+            data=rows;
+            res.render('pages/index', {
+                data:  data,
+                LatestDeviceStatus:rows[0].device_status
+              });
+        }
+
+    });
+
+    
+    console.log(data);
+      });
+
 // Close the database connection when the app is terminated
 app.on('close', () => {
     db.close((err) => {
@@ -100,7 +199,10 @@ app.on('close', () => {
     });
 });
 
+
+
 // Start the app on port 8080
 app.listen(8080, () => {
     console.log('Server is running on port 8080.');
+
 });
